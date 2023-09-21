3b3eb8631abfacfa7b361c403741f6c02f10f1cb
CokeLee777 <akak640@kyonggi.ac.kr>
2023-09-21 20:20:41 +0900

    fix: fix devops file syntax

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,55 +1,30 @@
 #!/bin/bash
 
+export ECR_REGISTRY=${ECR_REGISTRY}
+export AWS_ECR_REPOSITORY=${AWS_ECR_REPOSITORY}
+export IMAGE_TAG=${IMAGE_TAG}
+
 # Update AWS EKS user config
 aws eks update-kubeconfig --region ap-northeast-2 --name $AWS_EKS_CLUSTER_NAME
 
 # Deploy kubernetes deployment resource
-if ! kubectl get deployment config-deployment &> /dev/null; then
-  echo "Create new kubernetes deployment resources..."
-  kubectl create -f ./deployment-prod.yml
-else
-  echo "Apply new docker image "
-  kubectl set image deployments/config-deployment config-service=$ECR_REGISTRY/$AWS_ECR_REPOSITORY:$IMAGE_TAG --record
-fi
+echo "Apply new kubernetes deployment resources..."
+envsubst < ./deployment-prod.yml | kubectl apply -f -
 
 # Check if deployment deploy is successful
-if ! kubectl get deployment config-deployment &> /dev/null; then
-  echo "Failed to create new kubernetes deployment resources"
-else
+if kubectl rollout status deployment/config-deployment | grep -q "successfully rolled out"; then
   echo "Success to create new kubernetes deployment resources"
 
-  # Check if the rollback was successful
-  if ! kubectl rollout status deployment/config-deployment --watch=false | grep -q "successfully rolled out"; then
-    echo "Rolling update has failed. Initiating rollback..."
+  echo "Apply new kubernetes Service resources..."
+  kubectl apply -f ./service-prod.yml
+else
+  echo "Failed to create new kubernetes deployment resources"
+  echo "Rolling update has failed. Initiating rollback..."
 
-    kubectl rollout undo deployment/config-deployment
-    kubectl rollout status deployment/config-deployment --watch=true
-
-    # Check one more time if the rollback was successful
-    if kubectl rollout status deployment/config-deployment --watch=false | grep -q "successfully rolled out"; then
-      echo "Rollback was successful."
-    else
-      echo "Rollback failed. Manual intervention required."
-    fi
+  kubectl rollout undo deployment/config-deployment
+  if kubectl rollout status deployment/config-deployment | grep -q "successfully rolled out"; then
+    echo "Rollback was successful"
   else
-    echo "Rolling update was successful. No rollback needed."
-
-    # Deploy kubernetes service resource
-    if ! kubectl get service config-service &> /dev/null; then
-      echo "Create new kubernetes service resources..."
-      kubectl create -f ./service-prod.yml
-    else
-      echo "Apply existing kubernetes service resources..."
-      kubectl apply -f ./service-prod.yml
-    fi
-
-    # Check if service deploy is successful
-    if ! kubectl get service config-service &> /dev/null; then
-      echo "Failed to create new kubernetes service resources"
-    else
-      echo "Success to create new kubernetes service resources"
-    fi
+    echo "Rollback failed. Manual intervention required."
   fi
 fi
-
-
